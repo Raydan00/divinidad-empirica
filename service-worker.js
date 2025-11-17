086f829cdd6e877686b40ad92718ec0224333b45
Raydan <170201671+Raydan00@users.noreply.github.com>
2025-11-17 15:24:19 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,40 +4,34 @@ const FILES_TO_CACHE = [
   '/index.html',
   '/style.css',
   '/manifest.json',
-  '/chapters.json',
-  // Puedes agregar más archivos si quieres pre-cachearlos
+  '/chapters.json'
 ];
 
-// Instalar service worker y cachear archivos
 self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(FILES_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
-// Activar service worker y limpiar cachés antiguas
 self.addEventListener('activate', (event) => {
   console.log('[ServiceWorker] Activate');
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
+      )
+    )
   );
   self.clients.claim();
 });
 
-// Interceptar requests y servir desde caché si está disponible
-self.addEventList
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
